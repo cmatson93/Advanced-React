585cf303e077bc86c9d818526e786990ed34f5ea
Christina Matson <cmatson93@gmail.com>
2018-10-18 16:15:08 -0700

    Meta component added

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-
+import Header from './Header';
+import Meta from './Meta';
 
 class Page extends Component {
     render() {
-        return (
-            <div>
-                <p>Hey I'm the page component</p>
-                {this.props.children}                
-            </div>
-        );
+        return <div>
+            <Meta />
+            <Header />
+            {this.props.children}
+          </div>;
     }
 }
 
